75e334b84a119d4485b903c47ed05c7daf226cb9
prosox <84389974+amvprosox@users.noreply.github.com>
2023-04-24 11:30:32 +0800

    Add files via upload

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
 
 export const Contact = () => {
   return (
+    <>
+    <Navbar />
     <section id="contactForm">
         <div className="container">
         <section class="">
             <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
                 <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
                 <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">We're here to help! If you have any questions or feedback, please don't hesitate to contact us. We'd love to hear from you.</p>
-                <form action="#" class="space-y-8">
+                <form action="#" class="space-y-8" netlify>
                     <div>
                         <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
                         <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
@@ -27,5 +30,6 @@ export const Contact = () => {
             </section>
         </div>
     </section>
+    </>
   )
 }
