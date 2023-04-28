70cf15fc43dc306a932a45340823676bc7628f50
prosox <84389974+amvprosox@users.noreply.github.com>
2023-04-28 09:46:34 +0800

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import { gsap } from "gsap";
 import { motion, AnimatePresence } from 'framer-motion';
@@ -86,61 +85,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 	</motion.div>
   </React.StrictMode>,
 )
-=======
-import React from 'react'
-import { gsap } from "gsap";
-import ReactDOM from 'react-dom/client'
-import Homepage from './pages/Homepage'
-import Experts from './pages/Experts/Experts'
-import { Rolan } from './pages/Experts/Members/Rolan'
-import { Prosox } from './pages/Experts/Members/Prosox'
-import { Olok } from './pages/Experts/Members/Olok'
-import { Jemuel } from './pages/Experts/Members/Jemuel'
-import './index.css'
-
-import { 
-	createBrowserRouter,
-	RouterProvider,
-	Route,
-} from "react-router-dom"
-import { Contact } from './pages/Contact/Contact';
-
-
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Homepage/>,
-	},
-	{
-		path: "experts",
-		element: <Experts/>,
-	},
-	{
-		path: "rolan",
-		element: <Rolan/>,
-	},
-	{
-		path: "prosox",
-		element: <Prosox/>,
-	},
-	{
-		path: "olok",
-		element: <Olok/>,
-	},
-	{
-		path: "jemuel",
-		element: <Jemuel/>,
-	},
-	{
-		path: "contact",
-		element: <Contact/>,
-	}
-])
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-	<RouterProvider router = {router} />
-  </React.StrictMode>,
-)
->>>>>>> 3acbac4da4f4cfdf65b88f17619fa7f8e1f65a23
